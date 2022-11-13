94b1a7be2686bfeb7532b95c5317f4bfab136441
stassie-lzrv <97458617+stassie-lzrv@users.noreply.github.com>
2022-11-13 18:49:13 +0300

    Add files via upload

---
+++ 5.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char *get_string(int *len, int *test) {
+    *len = 0; 
+    *test = 0;
+    int cp= 1; 
+    char *s = (char*) malloc(sizeof(char)); 
+    char c = getchar();
+    if(c > 127){
+      (*test)++;
+    }  
+    while (c != '\n') {
+        s[(*len)++] = c; 
+        if (*len >= cp) {
+            cp *= 2; 
+            s = (char*) realloc(s, cp * sizeof(char)); 
+        }
+
+        c = getchar();
+	if(c > 127){
+          (*test)++;
+        }          
+    }
+    s[*len] = '\0'; 
+    return s; 
+}
+void task(char *s, int len){
+  for(int i = 0; i < len;i++){
+        if((s[i] == (66) || s[i] == (67) || s[i] == (68) || s[i] == (70) || s[i] == (71) || s[i] == (72) || s[i] == (74) || s[i] == (75) || s[i] == (76) || s[i] == (77) || s[i] == (78) || s[i] == (80) || s[i] == (82) || s[i] == (83) || s[i] == (84) || s[i] == (86) || s[i] == (87) || s[i] == (88) || s[i] == (90)) || (s[i] == (66+32) || s[i] == (67+32) || s[i] == (68+32) || s[i] == (70+32) || s[i] == (71+32) || s[i] == (72+32) || s[i] == (74+32) || s[i] == (75+32) || s[i] == (76+32) || s[i] == (77+32) || s[i] == (78+32) || s[i] == (80+32) || s[i] == (82+32) || s[i] == (83+32) || s[i] == (84+32) || s[i] == (86+32) || s[i] == (87+32) || s[i] == (88+32) || s[i] == (90+32))){ 
+	  printf("%d", s[i]);
+        }
+	else{
+          printf("%c", s[i]);
+        }
+  }
+}
+
+int main() {
+    int len, test; 
+    char *s = get_string(&len, &test); 
+    if(test == 0){
+      task(s, len);
+      return 0; 
+    }
+    else{
+      printf("incorrect input");
+      return 0;
+    } 
+    free(s); 
+    return 0;
+}
---
+++ 5.s
@@ -0,0 +1,441 @@
+	.file	"5.c"
+	.text
+	.globl	get_string
+	.type	get_string, @function
+get_string:
+.LFB5:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$32, %rsp
+	movq	%rdi, -24(%rbp)			#*len
+	movq	%rsi, -32(%rbp)			#*test
+	movq	-24(%rbp), %rax
+	movl	$0, (%rax)			#*len = 0
+	movq	-32(%rbp), %rax
+	movl	$0, (%rax)			#*test = 0
+	movl	$1, -12(%rbp)
+	movl	$1, %edi			#cp = 1
+	call	malloc@PLT
+	movq	%rax, -8(%rbp)			#*s
+	call	getchar@PLT
+	movb	%al, -13(%rbp)			#c 
+	jmp	.L2
+.L4:
+	movq	-24(%rbp), %rax
+	movl	(%rax), %eax
+	leal	1(%rax), %ecx
+	movq	-24(%rbp), %rdx
+	movl	%ecx, (%rdx)
+	movslq	%eax, %rdx
+	movq	-8(%rbp), %rax
+	addq	%rax, %rdx
+	movzbl	-13(%rbp), %eax
+	movb	%al, (%rdx)
+	movq	-24(%rbp), %rax
+	movl	(%rax), %eax
+	cmpl	%eax, -12(%rbp)
+	jg	.L3
+	sall	-12(%rbp)
+	movl	-12(%rbp), %eax
+	movslq	%eax, %rdx
+	movq	-8(%rbp), %rax
+	movq	%rdx, %rsi
+	movq	%rax, %rdi
+	call	realloc@PLT
+	movq	%rax, -8(%rbp)
+.L3:
+	call	getchar@PLT
+	movb	%al, -13(%rbp)
+.L2:
+	cmpb	$10, -13(%rbp)
+	jne	.L4
+	movq	-24(%rbp), %rax
+	movl	(%rax), %eax
+	movslq	%eax, %rdx
+	movq	-8(%rbp), %rax
+	addq	%rdx, %rax
+	movb	$0, (%rax)
+	movq	-8(%rbp), %rax			#return s
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE5:
+	.size	get_string, .-get_string
+	.section	.rodata
+.LC0:
+	.string	"%d"
+	.text
+	.globl	task
+	.type	task, @function
+task:
+.LFB6:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$32, %rsp
+	movq	%rdi, -24(%rbp)			#*s
+	movl	%esi, -28(%rbp)			#len 
+	movl	$0, -4(%rbp)			#int i = 0
+	jmp	.L7
+.L11:
+	movl	-4(%rbp), %eax
+	movslq	%eax, %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movzbl	(%rax), %eax
+	cmpb	$66, %al
+	je	.L8
+	movl	-4(%rbp), %eax
+	movslq	%eax, %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movzbl	(%rax), %eax
+	cmpb	$67, %al
+	je	.L8
+	movl	-4(%rbp), %eax
+	movslq	%eax, %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movzbl	(%rax), %eax
+	cmpb	$68, %al
+	je	.L8
+	movl	-4(%rbp), %eax
+	movslq	%eax, %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movzbl	(%rax), %eax
+	cmpb	$70, %al
+	je	.L8
+	movl	-4(%rbp), %eax
+	movslq	%eax, %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movzbl	(%rax), %eax
+	cmpb	$71, %al
+	je	.L8
+	movl	-4(%rbp), %eax
+	movslq	%eax, %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movzbl	(%rax), %eax
+	cmpb	$72, %al
+	je	.L8
+	movl	-4(%rbp), %eax
+	movslq	%eax, %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movzbl	(%rax), %eax
+	cmpb	$74, %al
+	je	.L8
+	movl	-4(%rbp), %eax
+	movslq	%eax, %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movzbl	(%rax), %eax
+	cmpb	$75, %al
+	je	.L8
+	movl	-4(%rbp), %eax
+	movslq	%eax, %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movzbl	(%rax), %eax
+	cmpb	$76, %al
+	je	.L8
+	movl	-4(%rbp), %eax
+	movslq	%eax, %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movzbl	(%rax), %eax
+	cmpb	$77, %al
+	je	.L8
+	movl	-4(%rbp), %eax
+	movslq	%eax, %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movzbl	(%rax), %eax
+	cmpb	$78, %al
+	je	.L8
+	movl	-4(%rbp), %eax
+	movslq	%eax, %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movzbl	(%rax), %eax
+	cmpb	$80, %al
+	je	.L8
+	movl	-4(%rbp), %eax
+	movslq	%eax, %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movzbl	(%rax), %eax
+	cmpb	$82, %al
+	je	.L8
+	movl	-4(%rbp), %eax
+	movslq	%eax, %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movzbl	(%rax), %eax
+	cmpb	$83, %al
+	je	.L8
+	movl	-4(%rbp), %eax
+	movslq	%eax, %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movzbl	(%rax), %eax
+	cmpb	$84, %al
+	je	.L8
+	movl	-4(%rbp), %eax
+	movslq	%eax, %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movzbl	(%rax), %eax
+	cmpb	$86, %al
+	je	.L8
+	movl	-4(%rbp), %eax
+	movslq	%eax, %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movzbl	(%rax), %eax
+	cmpb	$87, %al
+	je	.L8
+	movl	-4(%rbp), %eax
+	movslq	%eax, %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movzbl	(%rax), %eax
+	cmpb	$88, %al
+	je	.L8
+	movl	-4(%rbp), %eax
+	movslq	%eax, %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movzbl	(%rax), %eax
+	cmpb	$90, %al
+	je	.L8
+	movl	-4(%rbp), %eax
+	movslq	%eax, %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movzbl	(%rax), %eax
+	cmpb	$98, %al
+	je	.L8
+	movl	-4(%rbp), %eax
+	movslq	%eax, %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movzbl	(%rax), %eax
+	cmpb	$99, %al
+	je	.L8
+	movl	-4(%rbp), %eax
+	movslq	%eax, %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movzbl	(%rax), %eax
+	cmpb	$100, %al
+	je	.L8
+	movl	-4(%rbp), %eax
+	movslq	%eax, %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movzbl	(%rax), %eax
+	cmpb	$102, %al
+	je	.L8
+	movl	-4(%rbp), %eax
+	movslq	%eax, %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movzbl	(%rax), %eax
+	cmpb	$103, %al
+	je	.L8
+	movl	-4(%rbp), %eax
+	movslq	%eax, %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movzbl	(%rax), %eax
+	cmpb	$104, %al
+	je	.L8
+	movl	-4(%rbp), %eax
+	movslq	%eax, %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movzbl	(%rax), %eax
+	cmpb	$106, %al
+	je	.L8
+	movl	-4(%rbp), %eax
+	movslq	%eax, %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movzbl	(%rax), %eax
+	cmpb	$107, %al
+	je	.L8
+	movl	-4(%rbp), %eax
+	movslq	%eax, %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movzbl	(%rax), %eax
+	cmpb	$108, %al
+	je	.L8
+	movl	-4(%rbp), %eax
+	movslq	%eax, %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movzbl	(%rax), %eax
+	cmpb	$109, %al
+	je	.L8
+	movl	-4(%rbp), %eax
+	movslq	%eax, %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movzbl	(%rax), %eax
+	cmpb	$110, %al
+	je	.L8
+	movl	-4(%rbp), %eax
+	movslq	%eax, %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movzbl	(%rax), %eax
+	cmpb	$112, %al
+	je	.L8
+	movl	-4(%rbp), %eax
+	movslq	%eax, %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movzbl	(%rax), %eax
+	cmpb	$114, %al
+	je	.L8
+	movl	-4(%rbp), %eax
+	movslq	%eax, %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movzbl	(%rax), %eax
+	cmpb	$115, %al
+	je	.L8
+	movl	-4(%rbp), %eax
+	movslq	%eax, %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movzbl	(%rax), %eax
+	cmpb	$116, %al
+	je	.L8
+	movl	-4(%rbp), %eax
+	movslq	%eax, %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movzbl	(%rax), %eax
+	cmpb	$118, %al
+	je	.L8
+	movl	-4(%rbp), %eax
+	movslq	%eax, %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movzbl	(%rax), %eax
+	cmpb	$119, %al
+	je	.L8
+	movl	-4(%rbp), %eax
+	movslq	%eax, %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movzbl	(%rax), %eax
+	cmpb	$120, %al
+	je	.L8
+	movl	-4(%rbp), %eax
+	movslq	%eax, %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movzbl	(%rax), %eax
+	cmpb	$122, %al
+	jne	.L9
+.L8:
+	movl	-4(%rbp), %eax
+	movslq	%eax, %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movzbl	(%rax), %eax
+	movsbl	%al, %eax
+	movl	%eax, %esi
+	leaq	.LC0(%rip), %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	jmp	.L10
+.L9:
+	movl	-4(%rbp), %eax
+	movslq	%eax, %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movzbl	(%rax), %eax
+	movsbl	%al, %eax
+	movl	%eax, %edi
+	call	putchar@PLT
+.L10:
+	addl	$1, -4(%rbp)
+.L7:
+	movl	-4(%rbp), %eax			#void func
+	cmpl	-28(%rbp), %eax
+	jl	.L11
+	nop
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE6:
+	.size	task, .-task
+	.section	.rodata
+.LC1:
+	.string	"incorrect input"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB7:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$32, %rsp
+	movq	%fs:40, %rax
+	movq	%rax, -8(%rbp)
+	xorl	%eax, %eax
+	leaq	-20(%rbp), %rdx		#len 
+	leaq	-24(%rbp), %rax		#test
+	movq	%rdx, %rsi
+	movq	%rax, %rdi
+	call	get_string
+	movq	%rax, -16(%rbp)		#s from get_string
+	movl	-20(%rbp), %eax
+	testl	%eax, %eax
+	jne	.L13
+	movl	-24(%rbp), %edx		#s to task
+	movq	-16(%rbp), %rax		#len to task
+	movl	%edx, %esi
+	movq	%rax, %rdi
+	call	task
+	movl	$0, %eax		#void func
+	jmp	.L15
+.L13:
+	leaq	.LC1(%rip), %rdi	#string to func printf
+	movl	$0, %eax
+	call	printf@PLT
+	movl	$0, %eax
+.L15:
+	movq	-8(%rbp), %rcx
+	xorq	%fs:40, %rcx
+	je	.L16
+	call	__stack_chk_fail@PLT
+.L16:
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE7:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 7.5.0-3ubuntu1~18.04) 7.5.0"
+	.section	.note.GNU-stack,"",@progbits
